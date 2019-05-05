cddc6fa5ab3a13cfd8a23c471c964297d0886b61
yylgit <394720943@qq.com>
2019-05-05 18:12:15 +0800

    init

---
+++ packages/cml-tt-project/chameleon.config.js
@@ -0,0 +1,57 @@
+
+// 设置静态资源的线上路径
+const publicPath = '//www.static.chameleon.com/cml';
+// 设置api请求前缀
+const apiPrefix = 'https://api.chameleon.com';
+const path = require('path');
+
+cml.config.merge({
+  templateLang: "cml",
+  templateType: "html",
+  builtinNpmName: 'cml-tt-ui-builtin',
+  extPlatform: {
+    tt: 'cml-tt-plugin',
+  },
+  babelPath: [
+    path.join(__dirname,'node_modules/cml-tt-ui-builtin'),
+    path.join(__dirname,'node_modules/cml-tt-runtime'),
+    path.join(__dirname,'node_modules/cml-tt-api'),
+    path.join(__dirname,'node_modules/cml-tt-ui'),
+    path.join(__dirname,'node_modules/cml-tt-store'),
+  ],
+  platforms: ["web","weex","wx","alipay","baidu"],
+  buildInfo: {
+    wxAppId: '123456'
+  },
+  wx: {
+    dev: {
+    },
+    build: {
+      apiPrefix
+    }
+  },
+  web: {
+    dev: {
+      analysis: false,
+      console: false
+    },
+    build: {
+      analysis: false,
+      publicPath: `${publicPath}/web/`,
+      apiPrefix
+    }
+  },
+  weex: {
+    dev: {
+    },
+    build: {
+      publicPath: `${publicPath}/weex/`,
+      apiPrefix
+    },
+    custom: {
+      publicPath: `${publicPath}/wx/`,
+      apiPrefix
+    }
+  }
+})
+
