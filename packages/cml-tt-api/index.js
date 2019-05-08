d68272c6b138524ef1b6fd4bf4b36472457fdeec
jeany li <jeanyli@jeanydeiMac.local>
2019-05-08 17:41:11 +0800

    15

--- packages/cml-tt-api/index.js
+++ packages/cml-tt-api/index.js
@@ -1,4 +1,35 @@
 import alert from './src/interfaces/alert/index.js';
+import confirm from './src/interfaces/confirm/index.js';
+import showToast from './src/interfaces/showToast/index.js';
+import setTitle from './src/interfaces/setTitle/index.js';
+import requestAnimationFrame from './src/interfaces/requestAnimationFrame/index.js';
+import cancelAnimationFrame from './src/interfaces/cancelAnimationFrame/index.js';
+import inSDK from './src/interfaces/inSDK/index.js';
+import chooseImage from './src/interfaces/chooseImage/index.js';
+import close from './src/interfaces/close/index.js';
+import setStorage from './src/interfaces/setStorage/index.js';
+import getStorage from './src/interfaces/getStorage/index.js';
+import removeStorage from './src/interfaces/removeStorage/index.js';
+import getClipBoardData from './src/interfaces/getClipBoardData/index.js';
+import setClipBoardData from './src/interfaces/setClipBoardData/index.js';
+import open from './src/interfaces/open/index.js';
+// import createAnimation from './src/interfaces/createAnimation2/index.js.js';
+
 export default {
-  alert
+  alert,
+  confirm,
+  showToast,
+  setTitle,
+  requestAnimationFrame,
+  cancelAnimationFrame,
+  inSDK,
+  chooseImage,
+  close,
+  setStorage,
+  getStorage,
+  removeStorage,
+  getClipBoardData,
+  setClipBoardData,
+  open
+  // createAnimation
 }
---
+++ packages/cml-tt-api/src/interfaces/createAnimation2/index.js
@@ -0,0 +1,7 @@
+import Index from './index.interface';
+import CreateAnimationFactory from 'chameleon-api/src/interfaces/createAnimation/createAnimationFactory';
+
+console.log(Index)
+Index.initAnimation();
+
+// export default (description) => new CreateAnimationFactory(description);
