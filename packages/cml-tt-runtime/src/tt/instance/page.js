7969fa733ce2a31c0c17cb1171b9de56dd07e492
metajs <zycode277@gmail.com>
2019-05-14 10:30:44 +0800

    :sparkles: implement page

---
+++ packages/cml-tt-runtime/src/tt/instance/page.js
@@ -0,0 +1,59 @@
+import BaseCtor from '../../common/proto/BaseCtor'
+import lifecycle from '../../common/util/lifecycle'
+import VmAdapter from '../core/VmAdapter'
+import MiniRuntimeCore from '../../common/proto/MiniRuntimeCore'
+
+export class Page extends BaseCtor {
+  constructor (options) {
+    super(options)
+
+    this.cmlType = 'tt'
+
+    const runtimeCore = new MiniRuntimeCore({
+      polyHooks: lifecycle.get('tt.page.polyHooks'),
+      platform: this.cmlType,
+      options: this.options
+    })
+
+    this.initVmAdapter(VmAdapter, {
+      options: this.options,
+      type: 'page',
+      runtimeMixins: {
+        onLoad() {
+          // 初始化
+          runtimeCore
+            .setContext(this)
+            .init()
+            .start('page-view-render')
+        },
+        onUnload() {
+          // stop
+          runtimeCore
+            .setContext(this)
+            .destory()
+        },
+        onPullDownRefresh() {
+          const path = this.route
+          
+          this.$cmlEventBus.emit(`${path}_onPullDownRefresh`, {
+            path
+          })
+        },
+        onReachBottom() {
+          const path = this.route
+          
+          this.$cmlEventBus.emit(`${path}_onReachBottom`, {
+            path
+          })
+        }
+      },
+      needResolveAttrs: ['methods'],
+      hooks: lifecycle.get('tt.page.hooks'),
+      hooksMap: lifecycle.get('tt.page.hooksMap'),
+      polyHooks: lifecycle.get('tt.page.polyHooks'),
+      usedHooks: lifecycle.get('tt.page.usedHooks')
+    })
+
+    __CML__GLOBAL.Page(this.options)
+  }
+}
