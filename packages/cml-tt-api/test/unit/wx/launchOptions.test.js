bb90818b8d02cc4d09bb869d7cad98b1a70200f7
JiM-W <550116990@qq.com>
2019-07-11 15:28:34 +0800

    删除无用代码 "

--- packages/cml-tt-api/test/unit/wx/launchOptions.test.js
+++
@@ -1,9 +0,0 @@
-require('../../vm/wx/index');
-var cml = require('../../.api/wx/index')["default"];
-var expect = require('chai').expect;
-
-describe('获得启动参数', function () {
-  it('getLaunchOptionsSync: 获得启动参数', function () {
-    expect('cml').to.equal(cml.getLaunchOptionsSync().query.name);
-  })
-})
