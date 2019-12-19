108ccef4343b7ce4a2ad304b78690f9c7cf436c4
JiM-W <550116990@qq.com>
2019-12-19 15:00:06 +0800

    feat: 增加eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    root:true,//
+    env: {
+      browser: true,
+      es6: true,
+    },
+    extends: [
+      'plugin:vue/base',
+      'standard',//由于团队实现eslint规范初期可能需要修改的地方比较多
+    ],
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly'
+    },
+    parserOptions: {
+      ecmaVersion: 2018,
+      parser:babel-eslint
+    },
+    rules: {
+      'vue/comment-directive': 'off',
+      'vue/jsx-uses-vars': 'off'
+    }
+  }
